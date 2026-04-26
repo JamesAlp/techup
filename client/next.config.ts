de99fefebeeb0f5ed0194ce26614c4afd652063b
James Alphonse <jamesalphonse95@gmail.com>
2026-04-26 17:40:16 -0400

    TEC-15 client nginx env checkpoint

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,19 +1,9 @@
-import { networkInterfaces } from 'node:os';
 import type { NextConfig } from 'next';
 
-const allowedDevOrigins = Array.from(
-  new Set(
-    Object.values(networkInterfaces())
-      .flat()
-      .filter((networkInterface): networkInterface is NonNullable<typeof networkInterface> => Boolean(networkInterface))
-      .filter(({ family, internal }) => family === 'IPv4' && !internal)
-      .map(({ address }) => address),
-  ),
-);
-
 const nextConfig: NextConfig = {
-  allowedDevOrigins,
-  reactStrictMode: false,
+  allowedDevOrigins: process.env.LOCAL_IP
+    ? process.env.LOCAL_IP.split(',')
+    : []
 };
 
 export default nextConfig;
--- scripts/run/dev/compose.sh
+++ scripts/run/dev/compose.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker compose -f docker-compose.dev.yml up
+docker compose --env-file .env.development -f docker-compose.dev.yml up --build
