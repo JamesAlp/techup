ebad34b493e6cb3746b4e41ac99ab94f9df58786
James Alphonse <jamesalphonse95@gmail.com>
2026-04-26 19:22:47 -0400

    TEC-15 server checkpoint

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const APP = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT;
+  const PORT = process.env.SERVER_PORT ?? 3001;
   await APP.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
